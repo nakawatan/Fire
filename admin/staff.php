f076b1b497aba6b15ad226fdfa05b193f6b90a5a
anjie03 <anjenette.mauhay@g.batstate-u.edu.ph>
2022-09-09 18:31:31 +0800

    staff

--- admin/staff.php
+++ admin/staff.php
@@ -399,7 +399,7 @@
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
                           <div class="pull-left m-0 font-weight-bold text-primary"> Users
-                          <button type="button" class="d-none d-sm-inline-block btn btn-sm btn-primary" data-toggle="modal" data-target="#exampleModal">Add New User</button>
+                          <a href="add_user.php"><button type="button" class="d-none d-sm-inline-block btn btn-sm btn-primary">Add New User</button></a>
                           </div>
                         </div>
                         <div class="card-body">
