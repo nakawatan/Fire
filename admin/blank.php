f1bbc4f092b199154f39820a57d78921770350fa
anjie03 <anjenette.mauhay@g.batstate-u.edu.ph>
2022-09-09 18:30:03 +0800

    second commit

--- admin/blank.php
+++ admin/blank.php
@@ -66,8 +66,8 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.php">Buttons</a>
-                        <a class="collapse-item" href="cards.php">Cards</a>
+                        <a class="collapse-item" href="blank.php">Buttons</a>
+                        <a class="collapse-item" href="blank.php">Cards</a>
                     </div>
                 </div>
             </li>
