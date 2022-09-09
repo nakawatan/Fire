21c04a1da0b553bb394b356c49acf8a730be89d9
anjie03 <anjenette.mauhay@g.batstate-u.edu.ph>
2022-09-09 18:30:49 +0800

    home page

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,35 @@
-<?php include 'header.php';?>
+<?php 
+session_start();
+
+
+if (isset($_SESSION['email']) && isset($_SESSION['password'])) { 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BFP R4A Mabini</title>
+    <link rel="icon" type="img/png" sizes="16x16" href="img/bfp.png">
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
 
 <body id="page-top">
 
@@ -44,8 +75,8 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.php">Fire Code Assessor</a>
-                        <a class="collapse-item" href="cards.php">Fire Code Fees</a>
+                        <a class="collapse-item" href="blank.php">Fire Code Assessor</a>
+                        <a class="collapse-item" href="blank.php">Fire Code Fees</a>
                     </div>
                 </div>
             </li>
@@ -569,12 +600,16 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.php">Logout</a>
+                    <a class="btn btn-primary" href="logout.php">Logout</a>
                 </div>
             </div>
         </div>
     </div>
-
+<?php 
+}else {
+   header("Location: login.php");
+}
+?>
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -595,3 +630,4 @@
 </body>
 
 </html>
+
