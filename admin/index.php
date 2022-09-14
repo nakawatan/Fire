6d4fb7a48e0e1e8d786bc71ce141eaabeb6b3817
anjie03 <anjenette.mauhay@g.batstate-u.edu.ph>
2022-09-14 17:53:44 +0800

    home page

--- admin/index.php
+++ admin/index.php
@@ -575,7 +575,7 @@ if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="logout.php">Logout</a>
+                    <a class="btn btn-primary" href="db_logout.php">Logout</a>
                 </div>
             </div>
         </div>
