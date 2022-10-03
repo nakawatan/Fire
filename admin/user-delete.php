f3c96a01897062135c602a2d882393bee829d2a6
anjie03 <anjenette.mauhay@g.batstate-u.edu.ph>
2022-10-03 11:18:16 +0800

    user

---
+++ admin/user-delete.php
@@ -0,0 +1,15 @@
+<?php 
+include 'db/db_con.php';
+if (isset($_GET['deleteid'])) {
+    $id = $_GET['deleteid'];
+
+    $sql = "DELETE FROM `user` WHERE id=$id";
+    $result = mysqli_query($con,$sql);
+    if ($result) {
+        echo "<script>alert('Do you really want to delete?');</script>";
+        echo "<script>window.location.href='user.php'</script>";
+    }
+
+}
+
+?>
