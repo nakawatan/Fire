f617d4bea9cdac0018c57156aa84eb5754d74dbc
anjie03 <anjenette.mauhay@g.batstate-u.edu.ph>
2022-11-28 17:16:10 +0800

    carousel-caption

--- index.php
+++ index.php
@@ -53,10 +53,10 @@ $announcements = $announcement->get_records();
                                             echo "
                                             <div class='carousel-item ${active}'>
                                                 <img src='${image}' class='d-block w-100' alt=''>
-                                                <div>
-                                                <p><label>Date:</label> ${date}</p>
-                                                <p><label>Title:</label> ${rec['title']}</p>
-                                                <p><label>Details:</label> ${rec['detail']}</p>
+                                                <div class='carousel-caption d-none d-md-block'>
+                                                    <p><label>Date:</label> ${date}</p>
+                                                    <p><label>Title:</label> ${rec['title']}</p>
+                                                    <p><label>Details:</label> ${rec['detail']}</p>
                                                 </div>
                                             </div>
                                             ";
